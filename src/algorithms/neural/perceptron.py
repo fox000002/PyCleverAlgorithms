83f4c1a2d5caa5da1aafde98b72a53bb72b5b1d7
Yuansi Hu <Hu@Yuansis-MacBook-Pro.local>
2014-12-24 01:42:55 +0800

    Fixed UT of neural.perceptron.

--- src/algorithms/neural/perceptron.py
+++ src/algorithms/neural/perceptron.py
@@ -42,7 +42,7 @@ def get_output(weights, vector):
     return transfer(activation)
 
 
-def train_weight(weights, domain, num_inputs, iterations, lrate):
+def train_weights(weights, domain, num_inputs, iterations, lrate):
     for epoch in range(0, iterations):
         error = 0.0
         for pattern in domain:
@@ -58,16 +58,16 @@ def do_test_weights(weights, domain, num_inputs):
     correct = 0
     for pattern in domain:
         input_vector = [float(pattern[k]) for k in range(0, num_inputs)]
-    output = get_output(weights, input_vector)
-    if round(output) == pattern[-1]:
-        correct += 1
-    print("Finished test with a score of #{%d}/#{%d}" % (correct, len(domain)))
+        output = get_output(weights, input_vector)
+        if round(output) == pattern[-1]:
+            correct += 1
+    print("Finished test with a score of %d/%d" % (correct, len(domain)))
     return correct
 
 
 def execute(domain, num_inputs, iterations, learning_rate):
     weights = initialize_weights(num_inputs)
-    train_weight(weights, domain, num_inputs, iterations, learning_rate)
+    train_weights(weights, domain, num_inputs, iterations, learning_rate)
     do_test_weights(weights, domain, num_inputs)
     return weights
 
@@ -85,9 +85,10 @@ def main():
     print("weights : %s" % str(w))
     #
     print("result : %s -> %s" % (str([0, 1, 0]), get_output(w, [0, 1, 0])))
-    print("result : %s -> %s" % (str([0, 1, 1]), get_output(w, [0, 1, 1])))
+    print("result : %s -> %s" % (str([1, 1, 1]), get_output(w, [1, 1, 1])))
+    print("result : %s -> %s" % (str([1, 0, 0]), get_output(w, [1, 0, 0])))
+    print("result : %s -> %s" % (str([0, 0, 0]), get_output(w, [0, 0, 0])))
 
 
 if __name__ == "__main__":
     main()
-
--- src/algorithms/neural/tests/perceptron_test.py
+++ src/algorithms/neural/tests/perceptron_test.py
@@ -5,9 +5,88 @@ import os
 
 os.sys.path.append("..")
 
+from perceptron import iif, random_vector, initialize_weights, update_weights, activate, transfer, get_output, \
+    train_weights, do_test_weights, execute
+
+
 class TestPerceptron(unittest.TestCase):
     def setUp(self):
         pass
+
+    def test_iif(self):
+        self.assertEqual(iif(1 < 50, "1", "0"), "1")
+        self.assertEqual(iif('0' == '1', 1, 0), 0)
+
+    def test_random_vector(self):
+        minmax = [[1, 2], [2, 3]]
+
+        self.assertEqual(minmax[0][0], 1)
+
+        rv = random_vector(minmax)
+
+        self.assertEqual(len(rv), 2)
+        self.assertTrue(minmax[0][0] <= rv[0] <= minmax[0][1])
+        self.assertTrue(minmax[1][0] <= rv[1] <= minmax[1][1])
+
+    def test_initialize_weights(self):
+        w = initialize_weights(10)
+        self.assertEqual(11, len(w))
+        for v in w:
+            self.assertTrue(-1 <= v <= 1)
+
+    def test_update_weights(self):
+        # no error, no change, one inputs
+        w = [0.5, 0.5, 0.5]
+        update_weights(2, w, [1, 1], 1.0, 1.0, 0.9)
+        for x in w:
+            self.assertEqual(0.5, x)
+        # no error, no change, zero inputs
+        w = [0.5, 0.5, 0.5]
+        update_weights(2, w, [1, 1], 0.0, 0.0, 0.9)
+        for x in w:
+            self.assertEqual(0.5, x)
+        # an update
+        w = [0.5, 0.5, 0.5]
+        update_weights(2, w, [1, 1], 1.0, 0.0, 0.9)
+        for x in w:
+            self.assertEqual(1.4, x)
+
+    def test_activate(self):
+        self.assertEqual(5.0, activate([1.0, 1.0, 1.0, 1.0, 1.0], [1.0, 1.0, 1.0, 1.0]))
+        self.assertEqual(2.5, activate([0.5, 0.5, 0.5, 0.5, 0.5], [1.0, 1.0, 1.0, 1.0]))
+        self.assertEqual(-6.062263, activate([-6.072185, 2.454509, -6.062263], [0, 0]))
+
+    def test_transfer(self):
+        self.assertEqual(0, transfer(-1))
+        self.assertEqual(1, transfer(0))
+        self.assertEqual(1, transfer(1))
+
+    def test_get_output(self):
+        self.assertEqual(1, get_output([1, 1, 1], [1, 1]))
+        self.assertEqual(0, get_output([-1, -1, -1], [1, 1]))
+
+    def test_train_weights(self):
+        domain = [[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]]
+        w = [-1, -1, -1]
+        train_weights(w, domain, 2, 10, 0.5)
+        for x in w:
+            self.assertNotEqual(-1, x)
+
+    def test_test_weights(self):
+        rs = None
+        domain = [[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]]
+        w = [0.5, 0.5, -0.5]
+        rs = do_test_weights(w, domain, 2)
+        self.assertEqual(4, rs)
+
+    def test_search(self):
+        domain = [[0,0,0], [0,1,1], [1,0,1], [1,1,1]]
+        weights = None
+        weights = execute(domain, 2, 20, 0.1)
+        self.assertEqual(3, len(weights))
+        rs = None
+        rs = do_test_weights(weights, domain, 2)
+        self.assertEqual(4, rs)
 
 if __name__ == '__main__':
     unittest.main()
